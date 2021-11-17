5acebf99ead180fbf1ea93391d7582c1c7be1ed2
braydenwonglee <braydenlwong@gmail.com>
2021-11-17 12:34:27 -0700

    Registering an account is fully functional

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const expressSession = require("express-session");
+const routes = require('./routes/routes.js');
 const pug = require("pug");
+const path = require('path');
 const express = require("express");
 const app = express();
 
@@ -12,9 +14,11 @@ const urlencodedParser = express.urlencoded({
     extended: false
 });
 
-app.get("/",routes.index);
+app.get("/", routes.index);
 app.get("/login",routes.login);
 app.get("/register",routes.register);
+
+app.post('/postRegister', urlencodedParser, routes.postRegister);
 
 
 
