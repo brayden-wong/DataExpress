28c91c15782657d188af7fa1ea754ff1ddebd632
jedominguez <78039980+YELO-icy@users.noreply.github.com>
2021-12-08 10:09:31 -0700

    part 2
    pt 2

--- Front-end/script.js
+++ Front-end/script.js
@@ -5,40 +5,80 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 600;
 
-const drawData = () => {
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    });
 
-    const loop = () =>{
+let question1Info = {
+    x : 0,
+    y : 0,
+    w: 10,
+    height: 30,
+    color: "#530"
+}
+
+let question1Info2 = {
+    x: 0,
+    y: 100,
+    w: 10,
+    height: 30, 
+    color: "#111"
+}
+
+let question1Info3 = {
+    x: 0,
+    y: 200, 
+    w: 10,
+    height: 30,
+    color: "#0ff"
+}
+
+
+
+const drawData = (data) => {
+    let dragon = 0
+    let rabbit = 0
+    let dog = 0;
+    let bull = 0
+
+    const loop = (num) =>{
         
+        for(let i = 0; i < data.length; i++)
+        {
+            if(i == "Dragon"){ dragon += 1 }
+            if(i == "Rabbit"){ rabbit += 1 }
+            if(i == "Dog"){ dog += 1 }
+            if(i == "Bull"){ bull += 1 }
+        }
+        
+        if(num == 1){
+            ctx.fillStyle = question1Info.color
+            ctx.fillRect(question1Info.x,question1Info.y,question1Info.w,question1Info.height)
+            question1Info.w += dragon * 100
+        }
+
+        if(num == 2){
+            ctx.fillStyle = question1Info2.color
+            ctx.fillRect(question1Info2.x,question1Info2.y,question1Info2.w,question1Info2.height)
+            question1Info2.w += rabbit * 100
+        }
+        if(num == 3){
+            ctx.fillStyle = question1Info3.color
+            ctx.fillRect(question1Info3.x,question1Info3.y,question1Info3.w,question1Info3.height)
+            question1Info3.w += dog * 100
+        }
+        if(num == 4){
+            if(question1Info.w > 0) question1Info.x = bull - question1Info.height 
+        }
     };
 
-
-    let question1Info = {
-        x : 0,
-        y : 110,
-        size: 40,
-        color: "#530"
-    }
-
-    let question2Info = {
-        x: 10,
-        y: 120,
-        size: 40, 
-        color: "#111"
-    }
-
-    let question3Info = {
-        x: 20,
-        y: 130, 
-        size: 40,
-        color: "#0ff"
-    }
-
-
+    setInterval(loop(1),10)
+    setInterval(loop(2),10)
+    setInterval(loop(3),10)
 };
 
-canvas.addEventListener("load",drawData);
+
+
+fetch(url)
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+    drawData(data)
+});
